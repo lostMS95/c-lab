8862913f1ab53ffa7066ae991cd3b9e045c1c17f
gwpark <rmsdn0415@naver.com>
2024-09-02 22:17:57 +0900

    -웹소켓 개발중

--- src/main/java/com/ms/clab/config/WebSocketConfig.java
+++ src/main/java/com/ms/clab/config/WebSocketConfig.java
@@ -21,6 +21,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
-
     }
 }
